0679238ff015c8e33faaa6f440ac5e7fac296a21
johannpa <johann_paque@hotmail.fr>
2024-04-28 12:56:04 +0200

    Setup

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 
+setupCanvas();
+window.addEventListener("resize", setupCanvas);
+
+function setupCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
 // intro motion
 let mouseMoved = false;
 
